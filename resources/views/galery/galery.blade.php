5b504200115c62a74c8a4814739672e6338ca264
nurulsilpia <nurulsilpia3107@gmail.com>
2022-02-21 21:11:42 +0700

    test push

--- resources/views/galery/galery.blade.php
+++ resources/views/galery/galery.blade.php
@@ -11,7 +11,6 @@
                 <img src="https://source.unsplash.com/500x400?library" class="card-img-top" alt="...">
                 <div class="card-body">
                   <h5 class="card-title">Card title</h5>
-                  <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                 </div>
               </div>
             </div>
@@ -20,7 +19,6 @@
                 <img src="https://source.unsplash.com/500x400?library" class="card-img-top" alt="...">
                 <div class="card-body">
                   <h5 class="card-title">Card title</h5>
-                  <p class="card-text">This is a short card.</p>
                 </div>
               </div>
             </div>
@@ -29,7 +27,6 @@
                 <img src="https://source.unsplash.com/500x400?library" class="card-img-top" alt="...">
                 <div class="card-body">
                   <h5 class="card-title">Card title</h5>
-                  <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
                 </div>
               </div>
             </div>

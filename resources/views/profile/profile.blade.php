9f724b3269176d38428910bcbbb326a0835fb55e
nurulsilpia <nurulsilpia3107@gmail.com>
2022-02-23 10:43:20 +0700

    rev halaman profile

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -1,94 +1,147 @@
 @extends('layouts.main')
 @section('content')
-    <section class="section">
-        <div class="section-header">
+<section class="section">
+    <div class="section-header d-block">
+        <div class="mb-5 text-center">
             <h1>Tentang Kami, Perpustakaan SMK Wikrama</h1>
         </div>
 
-        <div class="card mb-3">
-        <img src="	assets/img/profile.png"  height="500px" alt="desain web">
+        <img src="assets/img/profile.png" class="img-fluid rounded mx-auto d-block" height="500px" alt="desain web">
         
         <div class="px-5 mt-5">
-        <h4 class=" text-primary">
-                    Identitas Perpustakaan
-        </h4>
-                <ul class="list-unstyled text-dark">
-                <li>Nama Perpustakaan : Perpustakaan SMK Wikrama</li>
-                <li>Nama Kepala Perpustakaan : Jakaria, S.E</li>
-                <li>Nama Pustakawati : Danang Andrian Wicaksana</li>
-                <li>Nomor SK Pendirian Perpustakaan : 421.5/0036/SMK Wikrama/IX/2008</li>
-        <br>
-                <li>Alamat : Jl. Raya Wangun Kel. Sindangsari Bogor</li>
-                <li>Kecamatan : Bogor Timur</li>
-                <li>Kabupaten / Kota : Bogor</li>
-                <li>Propinsi : Jawa Barat</li>
-                <li>Kode Pos : 16720</li>
-        <br>
-                <li>Telepon & Faksimili : (0251) 8242411</li>
-                <li>E-mail : prohumasi@smkwikrama.net</li>
-        <br>
-                <li>Luas Gedung Perpustakaan                                : 96 m2</li>
-                <li>Status Kepemilkan Gedung Perpustakaan    : Milik Yayasan Prawitama</li>
-                <li>Lokasi Perpustakaan                                             : Jarak di Pusat kegiatan pembelajaran</li>
-        <br>
-        <hr>
-        <br>
+            <h4 class=" text-primary">Identitas Perpustakaan</h4>
+            <table class="mb-5">
+                <tr>
+                    <th>Nama Perpustakaan</th>
+                    <td class="px-3">:</td>
+                    <td>Perpustakaan SMK Wikrama</td>
+                </tr>
+                <tr>
+                    <th>Nama Kepala Perpustakaan</th>
+                    <td class="px-3">:</td>
+                    <td>Jakaria, S.E</td>
+                </tr>
+                <tr>
+                    <th>Nama Pustakawati</th>
+                    <td class="px-3">:</td>
+                    <td>Danang Andrian Wicaksana</td>
+                </tr>
+                <tr>
+                    <th>Nomor SK Pendirian Perpustakaan</th>
+                    <td class="px-3">:</td>
+                    <td>421.5/0036/SMK Wikrama/IX/2008</td>
+                </tr>
 
-                <div>
-                <h4 class="text-primary">
-                    Sejarah Perpustakaan
+                <tr class="text-white">
+                    <th>.</th>
+                    <td class="px-3">:</td>
+                    <td>.</td>
+                </tr>
 
-                </h4>
-                <li>Perpustakaan SMK Wikrama Bogor berada di Kampus SMK Wikrama Bogor Kelurahan Sindangsari, 
-Kecamatan Bogor Timur, Kota Bogor didirikan pada tahun 1996.  Secara fisik perpustakaan sekolah 
-terletak di lantai 2 (dua) gedung Pajajaran yang merupakan gedung pertama yang dibangun, tepatnya 
-berada di area bangunan seluas 96 m2. Lokasi ini berada di pusat kegiatan pembelajaran yang mudah 
-dijangkau oleh peserta didik, pendidik dan tenaga kependidikan. Semenjak didirikan, keberadaan ruang 
-perpustakaan SMK Wikrama Bogor memberikan manfaat bagi sivitas akademik sekolah bahkan masyarakat 
-sekitar sekolah</li>
+                <tr>
+                    <th>Alamat</th>
+                    <td class="px-3">:</td>
+                    <td>Jl. Raya Wangun Kel. Sindangsari Bogor</td>
+                </tr>
+                <tr>
+                    <th>Kecamatan</th>
+                    <td class="px-3">:</td>
+                    <td>Bogor Timur</td>
+                </tr>
+                <tr>
+                    <th>Kabupaten / Kota</th>
+                    <td class="px-3">:</td>
+                    <td>Bogor</td>
+                </tr>
+                <tr>
+                    <th>Provinsi</th>
+                    <td class="px-3">:</td>
+                    <td>Jawa Barat</td>
+                </tr>
+                <tr>
+                    <th>Kode Pos</th>
+                    <td class="px-3">:</td>
+                    <td>16146</td>
+                </tr>
 
-                </div>
-                <br>
-                <hr>
-                <br>
-                <div>
+                <tr class="text-white">
+                    <th>.</th>
+                    <td class="px-3">:</td>
+                    <td>.</td>
+                </tr>
 
-                 <h4 class="text-primary">
-                    Visi,Misi Dan Motto Perpustakaan Serta Sasaran
-                </h4>
-                <br>
-                <h6 class="text-dark"> Visi : </h6>
- 
-                    <li>Menjadi perpustakaan sekolah teladan berbasis Teknologi Informasi dan Komunikasi</li>
-                    <br>
-                <h6 class="text-dark"> Misi : </h6>
-  
-                <li> Menjadikan perpustakaan sebagai sumber informasi bagi sivitas sekolah dan masyarakat </li>
-                <li>  Menjadikan budaya membaca sebagai bagian dari budaya sekolah</li>
-                    <br>
-                <h6 class="text-dark"> Motto : </h6>
-  
-                <li> “ Melayani dengan Hati dan Teknologi”</li>
-                <br>    
-                <h6 class="text-dark">Sasaran : </h6>
-                
-                    <li>1. Meningkatnya budaya membaca masyarakat sekolah</li>
-                    <li>2. Meningkatnya masyarakat pengunjung perpustakaan</li>
-                    <li>3. Meningkatnya pelayanan perpustakaan</li>
-                    <li>4. Terpenuhinya fungsi perpustakaan sebagai pusat informasi masyarakat sekolah</li>
-                <br>
-                <hr>
-                <br>
-                <h4 class="text-primary">
-                   Struktur Organisasi
-                </h4>
-                <img class="" src="assets/img/org.png"  height="400px" alt="desain web">
-                    
-                </div>
-            </div>
+                <tr>
+                    <th>Telepon & Faksimili</th>
+                    <td class="px-3">:</td>
+                    <td>(0251) 8242411</td>
+                </tr>
+
+                <tr class="text-white">
+                    <th>.</th>
+                    <td class="px-3">:</td>
+                    <td>.</td>
+                </tr>
+
+                <tr>
+                    <th>Luas Gedung Perpustakaan</th>
+                    <td class="px-3">:</td>
+                    <td>96 m<sup>2</sup></td>
+                </tr>
+                <tr>
+                    <th>Status Kepemilkan Gedung Perpustakaan</th>
+                    <td class="px-3">:</td>
+                    <td>Milik Yayasan Prawitama</td>
+                </tr>
+                <tr>
+                    <th>Lokasi Perpustakaan</th>
+                    <td class="px-3">:</td>
+                    <td>Jarak di Pusat kegiatan pembelajaran</td>
+                </tr>
+            </table>
         </div>
 
-    </section>
+        <hr>
+        <div class="mb-5 px-5">
+            <h4 class="text-primary">Sejarah Perpustakaan</h4>
+            <p class="text-justify">
+                Perpustakaan SMK Wikrama Bogor berada di Kampus SMK Wikrama Bogor Kelurahan Sindangsari, Kecamatan Bogor Timur, Kota Bogor didirikan pada tahun 1996.  Secara fisik perpustakaan sekolah terletak di lantai 2 (dua) gedung Pajajaran yang merupakan gedung pertama yang dibangun, tepatnya berada di area bangunan seluas 96 m2. Lokasi ini berada di pusat kegiatan pembelajaran yang mudah dijangkau oleh peserta didik, pendidik dan tenaga kependidikan. Semenjak didirikan, keberadaan ruang perpustakaan SMK Wikrama Bogor memberikan manfaat bagi sivitas akademik sekolah bahkan masyarakat sekitar sekolah
+            </p>
+        </div>
+        <hr>
 
-    
+        <div class="mb-5 px-5">
+            <h4 class="text-primary">Visi,Misi Dan Motto Perpustakaan Serta Sasaran</h4>
+            <h6 class="text-dark"> Visi : </h6>
+            <ul>
+                <li>Menjadi perpustakaan sekolah teladan berbasis Teknologi Informasi dan Komunikasi</li>
+            </ul>
+
+            <h6 class="text-dark"> Misi : </h6>
+            <ul>
+                <li> Menjadikan perpustakaan sebagai sumber informasi bagi sivitas sekolah dan masyarakat </li>
+                <li>  Menjadikan budaya membaca sebagai bagian dari budaya sekolah</li>
+            </ul>
+
+            <h6 class="text-dark"> Motto : </h6>
+            <blockquote class="blockquote text-italic">
+                <p>Melayani dengan Hati dan Teknologi</p>
+            </blockquote>
+
+            <h6 class="text-dark">Sasaran : </h6>
+            <ol>
+                <li>Meningkatnya budaya membaca masyarakat sekolah</li>
+                <li>Meningkatnya masyarakat pengunjung perpustakaan</li>
+                <li>Meningkatnya pelayanan perpustakaan</li>
+                <li>Terpenuhinya fungsi perpustakaan sebagai pusat informasi masyarakat sekolah</li>
+            </ol>
+        </div>
+        
+        <hr>
+        <div class="mb-5 px-5">
+            <h4 class="text-primary">Struktur Organisasi</h4>
+            <img class="" src="assets/img/org.png"  height="400px" alt="desain web">
+        </div>     
+
+    </div>
+</section>
 @endsection

987be29379777b89583ec8f643ed47ebfb77abeb
Millendra1 <millendramichel@gmail.com>
2022-02-23 09:06:22 +0700

    remake footer

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -18,7 +18,7 @@
                     <div class="card-body">
                        <h5 class="card-title">Judul</h5>
                        <p class="card-text">Some quick example text to build on the card </p>
-                       <a href="#" class="btn btn-primary">Preview</a>
+                       <a href="/buku-terbaru" class="btn btn-primary">Preview</a>
                     </div>
                 </div>
 
@@ -28,7 +28,7 @@
                              <div class="card-body">
                                 <h5 class="card-title">Judul</h5>
                                 <p class="card-text">Some quick example text to build on the card </p>
-                                <a href="#" class="btn btn-primary">Preview</a>
+                                <a href="/buku-terbaru" class="btn btn-primary">Preview</a>
                              </div>
                     </div>
                 </div>
@@ -39,7 +39,7 @@
                              <div class="card-body">
                                 <h5 class="card-title">Judul</h5>
                                 <p class="card-text">Some quick example text to build on the card </p>
-                                <a href="#" class="btn btn-primary">Preview</a>
+                                <a href="/buku-terbaru" class="btn btn-primary">Preview</a>
                              </div>
                     </div>
                 </div>
@@ -54,7 +54,7 @@
                     <div class="card-body">
                        <h5 class="card-title">Judul</h5>
                        <p class="card-text">Some quick example text to build on the card </p>
-                       <a href="#" class="btn btn-primary">Preview</a>
+                       <a href="/buku-terlaris" class="btn btn-primary">Preview</a>
                     </div>
                 </div>
 
@@ -64,7 +64,7 @@
                              <div class="card-body">
                                 <h5 class="card-title">Judul</h5>
                                 <p class="card-text">Some quick example text to build on the card </p>
-                                <a href="#" class="btn btn-primary">Preview</a>
+                                <a href="/buku-terlaris" class="btn btn-primary">Preview</a>
                              </div>
                     </div>
                 </div>
@@ -75,42 +75,15 @@
                              <div class="card-body">
                                 <h5 class="card-title">Judul</h5>
                                 <p class="card-text">Some quick example text to build on the card </p>
-                                <a href="#" class="btn btn-primary">Preview</a>
+                                <a href="/buku-terlaris" class="btn btn-primary">Preview</a>
                              </div>
                     </div>
                 </div>
         </div>
     </section>
-    
+
     <!-- FOOTER -->
-    <footer class="w-100 py-3 px-3 flex-shrink-0">
-        <div class="container py-4">
-            <div class="row gy-4 gx-5">
-                <div class="col-lg-4 col-md-6">
-                    <h5 class="h4 text-white">Mitra Kami  </h5><br>
-                    <ul class="list-unstyled text-muted">
-                        <li><a href="">SMK Wikrama Bogor</a></li>
-                        <li><a href="">SMK Wikrama 1 Garut</a></li>
-                        <li><a href="#">CV IDS Rumah Pendidikan</a></li>
-                        <li><a href="#"></a></li>
-                    </ul>
-                    <p class="small text-primary mb-0">&copy; Copyrights. 2018 Perpustakaan SMK Wikrama </p>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <h5 class="text-white mb-3">Contact Us </h5><br>
-                    <ul class="list-unstyled text-muted">
-                        <li><a href="#">prohumasi@smkwikrama.net</a></li>
-                        <li><a href="https://www.instagram.com/perpustakaan_wikrama/">@perpustakaan_wikrama</a></li>
-                        <br><li><a href="#">(0251) 8242411</a></li>
-                    </ul>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <h5 class="text-white mb-3">Grafik Pengunjung </h5>
-                    <p class="small text-muted"></p>
-                </div>
-            </div>
-        </div>
-    </footer>
+    
 </div>
 
 </div>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -36,9 +36,40 @@
       <div class="main-content">
         @yield('content')
       </div>
-          2.3.0
-        </div> --}}
+      <footer class="main-footer">
+      <div class="row gy-4 gx-5">
+                <div class="col-lg-4 col-md-6">
+                    <h5 class="h4 text-white">Mitra Kami  </h5><br>
+                    <ul class="list-unstyled text-muted">
+                        <li><a href="">SMK Wikrama Bogor</a></li>
+                        <li><a href="">SMK Wikrama 1 Garut</a></li>
+                        <li><a href="#">CV IDS Rumah Pendidikan</a></li>
+                        <li><a href="#"></a></li>
+                    </ul>
+                    <p class="small text-primary mb-0">&copy; Copyrights. 2018 Perpustakaan SMK Wikrama </p>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h5 class="text-white mb-3">Contact Us </h5><br>
+                    <ul class="list-unstyled text-muted">
+                        <li><a href="#">prohumasi@smkwikrama.net</a></li>
+                        <li><a href="https://www.instagram.com/perpustakaan_wikrama/">@perpustakaan_wikrama</a></li>
+                        <br><li><a href="#">(0251) 8242411</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 col-md-6">
+                    <h5 class="text-white mb-3">Clock </h5><br>
+                    <body onload="startTime()">
+                        <div id="clockdate">
+                            <div class="clockdate-wrapper">
+                                <div id="clock"></div>
+                                <div id="date"></div>
+                            </div>
+                        </div>
+                    </body>
+                </div>
+            </div>
       </footer>
+  
     </div>
   </div>
 
@@ -53,9 +84,47 @@
   <!-- Template JS File -->
   <script src="../assets/js/scripts.js"></script>
   <script src="../assets/js/custom.js"></script>
+  
 
   <!-- Father Icon -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js"></script>
+
+
+  <script>
+  function startTime() {
+  var today = new Date();
+  var hr = today.getHours();
+  var min = today.getMinutes();
+  var sec = today.getSeconds();
+  ap = (hr < 12) ? "<span>AM</span>" : "<span>PM</span>";
+  hr = (hr == 0) ? 12 : hr;
+  hr = (hr > 12) ? hr - 12 : hr;
+  
+  hr = checkTime(hr);
+  min = checkTime(min);
+  sec = checkTime(sec);
+  document.getElementById("clock").innerHTML = hr + ":" + min + ":" + sec + " " + ap;
+  
+  var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  var curWeekDay = days[today.getDay()];
+  var curDay = today.getDate();
+  var curMonth = months[today.getMonth()];
+  var curYear = today.getFullYear();
+  var date = curWeekDay+", "+curDay+" "+curMonth+" "+curYear;
+  document.getElementById("date").innerHTML = date;
+  
+  var time = setTimeout(function(){ startTime() }, 500);
+}
+function checkTime(i) {
+  if (i < 10) {
+      i = "0" + i;
+  }
+  return i;
+}
+  </script>
+
 </body>
 </html>
+

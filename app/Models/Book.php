e430cff2bcf9ff10fba02e609aea7f5c4d329221
nurulsilpia <nurulsilpia3107@gmail.com>
2022-02-25 18:24:25 +0700

    migration, controller & model category book

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,12 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\CategoryBook;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Book extends Model
 {
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    public function categoryBook()
+    {
+        return $this->belongsTo(CategoryBook::class);
+        // 1 buku punya 1 category. model Book sdh berelasi dg model CategoryBook
+    }
 }

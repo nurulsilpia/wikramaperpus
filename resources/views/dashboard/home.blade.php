77bfa7449dc9ebd5e57c987578bb331c4f41b282
nurulsilpia <nurulsilpia3107@gmail.com>
2022-03-06 14:09:33 +0700

    home user fix layout

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -1,91 +1,151 @@
 @extends('layouts.main')
 @section('content')
-    <section class="section d-flex">
-        <div class="section-header">
-           <img src="/img/banner.png" class="image" width="700px" height="350px" alt="thumbnail">
+<section class="section d-flex justify-content-around">
+    <div class="dashboard-left">
+        <div class="banner">
+            <img src="/img/banner.png" class="img-fluid rounded" width="700px" height="350px" alt="thumbnail">
         </div>
-    </section>
 
-    <h5 class="text-dark">Buku Terbaru</h5><br>
-    <section class="section d-flex">
-        <div class="section-header d-flex">
-               <div class="card " style="width: 14rem;">
-                 <img src="assets/img/cover.jpg" class="card-img-top"  alt="...">
-                    <div class="card-body">
-                       <h5 class="card-title">Judul</h5>
-                       <p class="card-text">Some quick example text to build on the card </p>
-                       <a href="/buku-terbaru" class="btn btn-primary">Preview</a>
-                    </div>
-                </div>
-
-                <div class="ml-3">
-                    <div class="card " style="width: 14rem;">
-                        <img src="assets/img/cover.jpg" class="card-img-top" alt="...">
-                             <div class="card-body">
-                                <h5 class="card-title">Judul</h5>
-                                <p class="card-text">Some quick example text to build on the card </p>
-                                <a href="/buku-terbaru" class="btn btn-primary">Preview</a>
-                             </div>
-                    </div>
-                </div>
-
-                <div class="ml-3">
-                    <div class="card " style="width: 14rem;">
-                        <img src="assets/img/cover.jpg" class="card-img-top" alt="...">
-                             <div class="card-body">
-                                <h5 class="card-title">Judul</h5>
-                                <p class="card-text">Some quick example text to build on the card </p>
-                                <a href="/buku-terbaru" class="btn btn-primary">Preview</a>
-                             </div>
-                    </div>
-                </div>
+        <div class="buku-terbaru my-5">
+            <h3 class="text-dark fw-bolder">Buku Terbaru</h3><br>
+            <div class="d-flex">
+                <div class="card " style="width: 15rem;">
+                  <img src="assets/img/cover.jpg" class="card-img-top"  alt="cover">
+                     <div class="card-body">
+                        <h5 class="card-title">Judul</h5>
+                        <p class="card-text">Some quick example text to build on the card </p>
+                        <a href="/buku-terbaru" class="btn btn-primary">Preview</a>
+                     </div>
+                 </div>
+ 
+                 <div class="ml-3">
+                     <div class="card " style="width: 15rem;">
+                         <img src="assets/img/cover.jpg" class="card-img-top"  alt="cover">
+                              <div class="card-body">
+                                 <h5 class="card-title">Judul</h5>
+                                 <p class="card-text">Some quick example text to build on the card </p>
+                                 <a href="/buku-terbaru" class="btn btn-primary">Preview</a>
+                              </div>
+                     </div>
+                 </div>
+ 
+                 <div class="ml-3">
+                     <div class="card " style="width: 15rem;">
+                         <img src="assets/img/cover.jpg" class="card-img-top"  alt="cover">
+                              <div class="card-body">
+                                 <h5 class="card-title">Judul</h5>
+                                 <p class="card-text">Some quick example text to build on the card </p>
+                                 <a href="/buku-terbaru" class="btn btn-primary">Preview</a>
+                              </div>
+                     </div>
+                 </div>
+            </div>
         </div>
-    </section>
 
-    <h5 class="text-dark">Buku Peminat Terbanyak</h5><br>
-    <section class="section d-flex">
-        <div class="section-header d-flex">
-               <div class="card " style="width: 14rem;">
-                 <img src="assets/img/cover.jpg" class="card-img-top" alt="...">
-                    <div class="card-body">
-                       <h5 class="card-title">Judul</h5>
-                       <p class="card-text">Some quick example text to build on the card </p>
-                       <a href="/buku-terlaris" class="btn btn-primary">Preview</a>
-                    </div>
-                </div>
-
-                <div class="ml-3">
-                    <div class="card " style="width: 14rem;">
-                        <img src="assets/img/cover.jpg" class="card-img-top" alt="...">
-                             <div class="card-body">
-                                <h5 class="card-title">Judul</h5>
-                                <p class="card-text">Some quick example text to build on the card </p>
-                                <a href="/buku-terlaris" class="btn btn-primary">Preview</a>
-                             </div>
-                    </div>
-                </div>
-
-                <div class="ml-3">
-                    <div class="card " style="width: 14rem;">
-                        <img src="assets/img/cover.jpg" class="card-img-top" alt="...">
-                             <div class="card-body">
-                                <h5 class="card-title">Judul</h5>
-                                <p class="card-text">Some quick example text to build on the card </p>
-                                <a href="/buku-terlaris" class="btn btn-primary">Preview</a>
-                             </div>
-                    </div>
-                </div>
+        <div class="peminat-terbanyak">
+            <h3 class="text-dark fw-bolder">Buku Peminat Terbanyak</h3><br>
+            <div class="d-flex">
+                <div class="card " style="width: 15rem;">
+                  <img src="assets/img/cover.jpg" class="card-img-top"  alt="cover">
+                     <div class="card-body">
+                        <h5 class="card-title">Judul</h5>
+                        <p class="card-text">Some quick example text to build on the card </p>
+                        <a href="/buku-terlaris" class="btn btn-primary">Preview</a>
+                     </div>
+                 </div>
+ 
+                 <div class="ml-3">
+                     <div class="card " style="width: 15rem;">
+                         <img src="assets/img/cover.jpg" class="card-img-top"  alt="cover">
+                              <div class="card-body">
+                                 <h5 class="card-title">Judul</h5>
+                                 <p class="card-text">Some quick example text to build on the card </p>
+                                 <a href="/buku-terlaris" class="btn btn-primary">Preview</a>
+                              </div>
+                     </div>
+                 </div>
+ 
+                 <div class="ml-3">
+                     <div class="card " style="width: 15rem;">
+                         <img src="assets/img/cover.jpg" class="card-img-top"  alt="cover">
+                              <div class="card-body">
+                                 <h5 class="card-title">Judul</h5>
+                                 <p class="card-text">Some quick example text to build on the card </p>
+                                 <a href="/buku-terlaris" class="btn btn-primary">Preview</a>
+                              </div>
+                     </div>
+                 </div>
+            </div>
         </div>
-    </section>
+    </div>
 
-    <!-- FOOTER -->
-    
-</div>
+    <div class="dashboard-right text-white rounded shadow-sm p-3 ml-3 col-3" style="background-color: #6777ef;">
+        <div id="chart" class="rounded">
 
+        </div>
+    </div>
+</section>
 
-
-
-    
-    
 @endsection
 
+@prepend('chart')
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script>
+        // Radialize the colors
+        Highcharts.setOptions({
+            colors: Highcharts.map(Highcharts.getOptions().colors, function (color) {
+                return {
+                    radialGradient: {
+                        cx: 0.5,
+                        cy: 0.3,
+                        r: 0.7
+                    },
+                    stops: [
+                        [0, color],
+                        [1, Highcharts.color(color).brighten(-0.3).get('rgb')] // darken
+                    ]
+                };
+            })
+        });
+
+        // Build the chart
+        Highcharts.chart('chart', {
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false,
+                type: 'pie'
+            },
+            title: {
+                text: 'Grafik Pengunjung'
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+            },
+            accessibility: {
+                point: {
+                    valueSuffix: '%'
+                }
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                        connectorColor: 'silver'
+                    }
+                }
+            },
+            series: [{
+                name: 'Jumlah',
+                data: [
+                    { name: 'Civitas', y: 61.41 },
+                    { name: 'Umum', y: 7.05 }
+                ]
+            }]
+        });
+                    
+    </script>
+@endprepend
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -71,43 +71,9 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/js/lightbox.min.js"></script>
 
-    <script>
-        function startTime() {
-          var today = new Date();
-          var hr = today.getHours();
-          var min = today.getMinutes();
-          var sec = today.getSeconds();
-          ap = (hr < 12) ? "<span>AM</span>" : "<span>PM</span>";
-          hr = (hr == 0) ? 12 : hr;
-          hr = (hr > 12) ? hr - 12 : hr;
-          
-          hr = checkTime(hr);
-          min = checkTime(min);
-          sec = checkTime(sec);
-          document.getElementById("clock").innerHTML = hr + ":" + min + ":" + sec + " " + ap;
-          
-          var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-          var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-          var curWeekDay = days[today.getDay()];
-          var curDay = today.getDate();
-          var curMonth = months[today.getMonth()];
-          var curYear = today.getFullYear();
-          var date = curWeekDay+", "+curDay+" "+curMonth+" "+curYear;
-          document.getElementById("date").innerHTML = date;
-          
-          var time = setTimeout(function(){ startTime() }, 500);
-        }
-        function checkTime(i) {
-          if (i < 10) {
-              i = "0" + i;
-          }
-          return i;
-        }
-    
-        //Data AOS
-        // AOS.init({
-        //   delay:500
-        // });
-    </script>
+    {{-- Highchart --}}
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+
+    @stack('chart')
 </body>
 </html>

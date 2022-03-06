7a2156a43d6dc063506834f08d03bb411e556081
nurulsilpia <nurulsilpia3107@gmail.com>
2022-03-06 20:42:36 +0700

    profile front end revisi

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.main')
 @section('content')
 <section class="section">
-    <div class="section-header d-block" style="font-size: 1rem;">
-        <div class="card rounded p-3">
+    <div class="section-header card d-block px-5">
+        <div class="rounded p-3">
             <div class="mb-4 text-center">
                 <h1><marquee>Tentang Kami, Perpustakaan SMK Wikrama</marquee></h1>
             </div>
@@ -10,7 +10,7 @@
                 <img src="/img/banner.png" class="img-fluid rounded d-block shadow-sm" height="500">
             </center>
         </div>
-        <div class="px-5 mt-3 card py-5">
+        <div class="px-5 mt-3">
             <h4 class=" text-primary">Identitas Perpustakaan</h4>
             <table class="mb-5">
                 <tr>
@@ -103,7 +103,7 @@
         </div>
 
         <hr><br>
-        <div class="mb-3 p-5 card">
+        <div class="mb-3 ">
             <h4 class="text-primary">Sejarah Perpustakaan</h4>
             <p class="text-justify">
                 Perpustakaan SMK Wikrama Bogor berada di Kampus SMK Wikrama Bogor Kelurahan Sindangsari, Kecamatan Bogor Timur, Kota Bogor didirikan pada tahun 1996.  Secara fisik perpustakaan sekolah terletak di lantai 2 (dua) gedung Pajajaran yang merupakan gedung pertama yang dibangun, tepatnya berada di area bangunan seluas 96 m2. Lokasi ini berada di pusat kegiatan pembelajaran yang mudah dijangkau oleh peserta didik, pendidik dan tenaga kependidikan. Semenjak didirikan, keberadaan ruang perpustakaan SMK Wikrama Bogor memberikan manfaat bagi sivitas akademik sekolah bahkan masyarakat sekitar sekolah
@@ -111,7 +111,7 @@
         </div>
         <hr><br>
 
-        <div class="mb-3 p-5 card">
+        <div class="mb-3 ">
             <h4 class="text-primary">Visi,Misi Dan Motto Perpustakaan Serta Sasaran</h4><br>
             <h6 class="text-dark"> Visi : </h6>
             <ul>
@@ -139,7 +139,7 @@
         </div>
         
         <hr><br>
-        <div class="mb-3 p-5 card">
+        <div class="mb-3 ">
             <h4 class="text-primary">Struktur Organisasi</h4>
             <img class="rounded mx-auto d-block" src="assets/img/org.png"  height="400px" alt="desain web">
         </div>     

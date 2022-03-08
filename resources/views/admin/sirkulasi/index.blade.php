b8155c2f86216d79ca8800d78b4f056436df6a4b
nurulsilpia <nurulsilpia3107@gmail.com>
2022-03-08 11:53:03 +0700

    sirkulasi denda

--- resources/views/admin/sirkulasi/index.blade.php
+++ resources/views/admin/sirkulasi/index.blade.php
@@ -28,10 +28,12 @@
               <th scope="col">Nama Siswa</th>
               <th scope="col">Kode Item</th>
               <th scope="col">Judul</th>
-              <th scope="col">Keterangan</th>
               <th scope="col">Kode Pinjam</th>
               <th scope="col">Tanggal Pinjam</th>
               <th scope="col">Tanggal Kembali</th>
+              <th scope="col">Lama Peminjaman</th>
+              <th scope="col">Keterangan</th>
+              <th scope="col">Ketelatan</th>
               <th scope="col">Denda</th>
               <th scope="col" colspan="2">Action</th>
             </tr>
@@ -43,12 +45,43 @@
                 <td> {{ $sirkulasi->siswa->nama }}</td>
                 <td> {{ $sirkulasi->kode_item }}</td>
                 <td> {{ $sirkulasi->book->judul }}</td>
-                <td> {{ $sirkulasi->keterangan }}</td>
                 <td> {{ $sirkulasi->kode_pinjam }}</td>
                 <td> {{ $sirkulasi->tanggal_pinjam }}</td>
                 <td> {{ $sirkulasi->tanggal_kembali }}</td>
                 <td>
-
+                  {{-- ini lama peminjaman --}}
+                  <?php
+                    $pinjam = new DateTime($sirkulasi->tanggal_pinjam);
+                    $kembali = new DateTime($sirkulasi->tanggal_kembali);
+                    $interval = $pinjam->diff($kembali)->format('%a days');
+                    echo $interval;
+                  ?>
+                </td>
+                <td> {{ $sirkulasi->keterangan }}</td>
+                <td>
+                  {{-- ini ketelatan --}}
+                  <?php
+                    $pinjam = new DateTime($sirkulasi->tanggal_pinjam);
+                    $kembali = new DateTime($sirkulasi->tanggal_kembali);
+                    $interval = $pinjam->diff($kembali);
+                    
+                    $today = new DateTime("now");
+                    $lengt = $today->diff($kembali)->format('%R%a days');
+                    
+                    echo $lengt;
+                  ?>
+                </td>
+                <td>
+                  <?php
+                    $pinjam = new DateTime($sirkulasi->tanggal_pinjam);
+                    $kembali = new DateTime($sirkulasi->tanggal_kembali);
+                    $interval = $pinjam->diff($kembali);
+                    
+                    $today = new DateTime("now");
+                    $lengt = $today->diff($kembali)->format('%a');
+                    
+                    echo 'Rp ' . $lengt * 500;
+                  ?>
                 </td>
                 <td>
                   <a href="/admin/sirkulasi/{{ $sirkulasi->id }}/edit" class="badge bg-warning text-white my-1"><i class="bi bi-pencil-square"></i></a>

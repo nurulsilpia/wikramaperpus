dd701e1207263c58088a26a4569d31d507fcb370
nurulsilpia <nurulsilpia3107@gmail.com>
2022-04-18 14:48:19 +0700

    navbar user logo

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,13 +1,13 @@
 <header class="l-header">
     <nav class="nav bd-grid">
         <div>
-            <p class="nav__logo">Perpustakaan Wikrama</p>
+            <p class="nav__logo"><img src="/img/logo.png" width="50" class="rounded-circle"><span class="ml-2">Perpustakaan Wikrama</span></p>
         </div>
 
         <div class="nav__menu" id="nav-menu">
             <ul class="nav__list list-unstyled m-0">
                 <li class="nav__item"><a href="/" class="nav__link {{ Request::is('/') ? 'active' : '' }}">Home</a></li>
-                <li class="nav__item"><a href="/ebook" class="nav__link {{ Request::is('ebook*') ? 'active' : '' }}">Ebook</a></li>
+                <li class="nav__item"><a href="/ebook" class="nav__link {{ Request::is('ebook*') ? 'active' : '' }}">Book</a></li>
                 <li class="nav__item"><a href="/profile-perpust" class="nav__link {{ Request::is('profile*') ? 'active' : '' }}">Profile</a></li>
                 <li class="nav__item"><a href="/posts" class="nav__link {{ Request::is('posts*') ? 'active' : '' }}">Article</a></li>
                 <li class="nav__item"><a href="/galery" class="nav__link {{ Request::is('galery*') ? 'active' : '' }}">Galery</a></li>

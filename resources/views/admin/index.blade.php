16405bb65b19b6f5cb853b498df4d5f9192d8805
nurulsilpia <nurulsilpia3107@gmail.com>
2022-05-29 12:16:15 +0700

    index admin

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,5 +6,51 @@
         <h1 class="h2">Welcomback, {{ auth()->user()->name }}</h1>
     </div>
 </div>
+<div class="section-body d-flex justify-content-between">
+    <div class="card text-danger" style="width: 15rem;">
+        <div class="card-body d-flex align-items-center justify-content-between">
+            <div>
+                <h5 class="card-title" style="font-size: 2,5rem;">Jumlah Buku</h5>
+                <p class="card-text text-center" style="font-size: 2rem;">{{ DB::table('books')->count() }}</p>
+            </div>
+            <div>
+                <i class="bi bi-book-half" style="font-size: 3rem;"></i>
+            </div>
+        </div>
+    </div>
+    <div class="card" style="width: 15rem;">
+        <div class="card-body d-flex align-items-center justify-content-between">
+            <div>
+                <h5 class="card-title" style="font-size: 2,5rem;">Jumlah User</h5>
+                <p class="card-text text-center" style="font-size: 2rem;">{{ DB::table('users')->count() }}</p>
+            </div>
+            <div>
+                <i class="bi bi-people-fill" style="font-size: 3rem;"></i>
+            </div>
+        </div>
+    </div>
+    <div class="card" style="width: 15rem;">
+        <div class="card-body d-flex align-items-center justify-content-between">
+            <div>
+                <h5 class="card-title text-warning" style="font-size: 2,5rem;">Jumlah Artikel</h5>
+                <p class="card-text text-center text-warning" style="font-size: 2rem;">{{ DB::table('posts')->count() }}</p>
+            </div>
+            <div>
+                <i class="bi bi-newspaper text-warning" style="font-size: 3rem;"></i>
+            </div>
+        </div>
+    </div>
+    <div class="card text-primary" style="width: 15rem;">
+        <div class="card-body d-flex align-items-center justify-content-between">
+            <div>
+                <h5 class="card-title" style="font-size: 2,5rem;">Jumlah Sirkulasi</h5>
+                <p class="card-text" style="font-size: 2rem;">{{ DB::table('sirkulasis')->count() }}</p>
+            </div>
+            <div>
+                <i class="bi bi-arrow-repeat" style="font-size: 3rem;"></i>
+            </div>
+        </div>
+    </div>
+</div>
 </section>
 @endsection
--- resources/views/sirkulasi/sirkulasi.blade.php
+++ resources/views/sirkulasi/sirkulasi.blade.php
@@ -25,47 +25,50 @@
             </tr>
           </thead>
           <tbody>
+            @if (Auth::user()->name)
+                
             @foreach (DB::table('sirkulasis')->get() as $key => $item)
-            <tr>
-                <td>{{ $key + 1 }}</td>
-                <td>{{ DB::table('users')->where('id', $item->user_id)->first()->name }}</td>
-                <td>{{ $item->kode_item }}</td>
-                <td>{{ DB::table('books')->where('id', $item->book_id)->first()->judul }}</td>
-                <td>{{ $item->kode_pinjam }}</td>
-                <td>{{ $item->tanggal_pinjam }}</td>
-                <td>{{ $item->tanggal_kembali }}</td>
-                @php
-
-                    $date1 = date_create($item->tanggal_pinjam);
-                    $date2 = date_create($item->tanggal_kembali);
-                    $diff = date_diff($date1,$date2);
-
-                @endphp
-                <td>{{ $diff->format("%R%a days")  }}</td>
-                <td>{{ $item->keterangan }}</td>
-                @php
-
-                    $date3 = new DateTime('now');
-                    $date4 = date_create($item->tanggal_kembali);
-                    $difff = date_diff($date3,$date4);
-
-                @endphp
-                <td>{{ $difff->format("%R%a days")  }}</td>
-                <td>
-                  {{-- ini denda --}}
+              <tr>
+                  <td>{{ $key + 1 }}</td>
+                  <td>{{ DB::table('users')->where('id', $item->user_id)->first()->name }}</td>
+                  <td>{{ $item->kode_item }}</td>
+                  <td>{{ DB::table('books')->where('id', $item->book_id)->first()->judul }}</td>
+                  <td>{{ $item->kode_pinjam }}</td>
+                  <td>{{ $item->tanggal_pinjam }}</td>
+                  <td>{{ $item->tanggal_kembali }}</td>
                   @php
-                    $pinjam = new DateTime($item->tanggal_pinjam);
-                    $kembali = new DateTime($item->tanggal_kembali);
-                    $interval = $pinjam->diff($kembali);
-                    
-                    $today = new DateTime("now");
-                    $lengt = $today->diff($kembali)->format('%a');
-                    
-                    echo 'Rp ' . $lengt * 500;
+
+                      $date1 = date_create($item->tanggal_pinjam);
+                      $date2 = date_create($item->tanggal_kembali);
+                      $diff = date_diff($date1,$date2);
+
                   @endphp
-                </td>
-            </tr>
+                  <td>{{ $diff->format("%R%a days")  }}</td>
+                  <td>{{ $item->keterangan }}</td>
+                  @php
+
+                      $date3 = new DateTime('now');
+                      $date4 = date_create($item->tanggal_kembali);
+                      $difff = date_diff($date3,$date4);
+
+                  @endphp
+                  <td>{{ $difff->format("%R%a days")  }}</td>
+                  <td>
+                    {{-- ini denda --}}
+                    @php
+                      $pinjam = new DateTime($item->tanggal_pinjam);
+                      $kembali = new DateTime($item->tanggal_kembali);
+                      $interval = $pinjam->diff($kembali);
+                      
+                      $today = new DateTime("now");
+                      $lengt = $today->diff($kembali)->format('%a');
+                      
+                      echo 'Rp ' . $lengt * 500;
+                    @endphp
+                  </td>
+              </tr>
             @endforeach
+            @endif
           </tbody>
         </table>
     </div>
